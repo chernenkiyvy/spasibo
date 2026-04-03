329c9f7aa5dbcc03eeb7841aaa6a55444d886a2f
chernenkiyvy <131249151+chernenkiyvy@users.noreply.github.com>
2026-04-03 18:37:48 +0700

    Add files via upload

--- admin/views/datesettings/view.html.php
+++ admin/views/datesettings/view.html.php
@@ -1,28 +1,11 @@
 <?php
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-// import Joomla view library
-jimport('joomla.application.component.view');
-
-class DatesettingsViewDatesettings extends JView {
-    function display($tpl = null) {
-            $items = $this -> get("Items");
-            $pagination = $this -> get("Pagination");
-
-            //Check for errors
-            if (count($errors = $this->get('Errors'))) 
-            {
-                JError::raiseError(500, implode('<br />', $errors));
-                return false;
-            }
-
-            // Assign data to the view
-            $this -> items = $items;
-            $this -> pagination = $pagination;
-
-            // Display the template
-            parent::display($tpl);
-
+class DatesettingsViewDatesettings extends JViewLegacy
+{
+    public function display($tpl = null)
+    {
+        JToolBarHelper::title('Настройка дат акции', 'calendar');
+        parent::display($tpl);
     }
 }
